ccfc9081f6dd7f8b2c6f1a790e4538ea598f276b
Temirlan Myrzakhmetov <temirlanmirzakhmetov@gmail.com>
2024-08-04 23:15:54 -0700

    fixes

--- src/lmnr/sdk/registry.py
+++ src/lmnr/sdk/registry.py
@@ -18,5 +18,12 @@ class Registry:
     def add(self, node_name: str, function: Callable[..., NodeInput]):
         self.functions[node_name] = NodeFunction(node_name, function)
 
+    def func(self, node_name: str):
+        def decorator(f: Callable[..., NodeInput]):
+            self.add(node_name, f)
+            return f
+
+        return decorator
+
     def get(self, node_name: str) -> Callable[..., NodeInput]:
         return self.functions[node_name].function
