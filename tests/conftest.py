393770fa918bc9570e694d759fde3f7bf84600b4
Din <dinmukhamed.mailibay@gmail.com>
2024-12-16 18:30:21 -0800

    update evals CLI and openllmetry versions

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,23 +1,37 @@
 import pytest
-
+from unittest.mock import patch
 from lmnr import Laminar
+from lmnr.openllmetry_sdk import Traceloop
+
 from opentelemetry.sdk.trace.export.in_memory_span_exporter import InMemorySpanExporter
-from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor, SpanExporter
 
 pytest_plugins = ("pytest_asyncio",)
 
 
 @pytest.fixture(scope="session")
-def exporter():
+def exporter() -> SpanExporter:
     exporter = InMemorySpanExporter()
     processor = SimpleSpanProcessor(exporter)
-    Laminar.initialize(
-        project_api_key="test_key",
-        _processor=processor,
-    )
+
+    # Set up a partial mock of Traceloop.init to inject our processor
+    orig_traceloop_init = Traceloop.init
+
+    def mock_traceloop_init(*args, **kwargs):
+        kwargs["processor"] = processor
+        orig_traceloop_init(*args, **kwargs)
+
+    with patch(
+        "lmnr.openllmetry_sdk.Traceloop.init",
+        side_effect=mock_traceloop_init,
+    ):
+        Laminar.initialize(
+            project_api_key="test_key",
+        )
+
     return exporter
 
 
-@pytest.fixture(autouse=True)
-def clear_exporter(exporter):
+@pytest.fixture(scope="function", autouse=True)
+def clear_exporter(exporter: InMemorySpanExporter):
     exporter.clear()
