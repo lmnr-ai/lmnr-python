4b9fe0cdad80b6b2a7a7bac52e7a39758d91bd68
Din <dinmukhamed.mailibay@gmail.com>
2025-05-28 15:49:58 +0100

    v0.6.4 allow tagging traces

--- src/lmnr/sdk/client/asynchronous/resources/__init__.py
+++ src/lmnr/sdk/client/asynchronous/resources/__init__.py
@@ -1,9 +1,11 @@
 from lmnr.sdk.client.asynchronous.resources.agent import AsyncAgent
 from lmnr.sdk.client.asynchronous.resources.browser_events import AsyncBrowserEvents
 from lmnr.sdk.client.asynchronous.resources.evals import AsyncEvals
+from lmnr.sdk.client.asynchronous.resources.tags import AsyncTags
 
 __all__ = [
     "AsyncAgent",
     "AsyncEvals",
     "AsyncBrowserEvents",
+    "AsyncTags",
 ]
