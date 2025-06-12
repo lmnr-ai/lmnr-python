b3ac53bdf41a7049b0337ff230aeb7dc9aa8ecd1
Dinmukhamed Mailibay <47117969+dinmukhamedm@users.noreply.github.com>
2025-06-12 17:43:15 +0100

    fix astreaming generator return in wrap pregel (#124)

--- tests/test_langchain.py
+++ tests/test_langchain.py
@@ -9,6 +9,8 @@ from langgraph.prebuilt import ToolNode, tools_condition
 from opentelemetry.sdk.trace.export.in_memory_span_exporter import InMemorySpanExporter
 from typing import Annotated, TypedDict
 
+import pytest
+
 
 class AgentState(TypedDict):
     messages: Annotated[list[AnyMessage], add_messages]
@@ -41,6 +43,211 @@ def test_langchain_langgraph(exporter: InMemorySpanExporter):
     graph = graph_builder.compile()
 
     graph.invoke({"messages": [HumanMessage(content="What is the weather in Tokyo?")]})
+
+    spans = exporter.get_finished_spans()
+    assert len(spans) == 3
+    workflow_span = [span for span in spans if span.name == "LangGraph.workflow"][0]
+    other_spans = [span for span in spans if span.name != "LangGraph.workflow"]
+    assert json.loads(
+        workflow_span.attributes["lmnr.association.properties.langgraph.nodes"]
+    ) == [
+        {
+            "id": "__start__",
+            "name": "__start__",
+            "metadata": None,
+        },
+        {
+            "id": "assistant",
+            "name": "assistant",
+            "metadata": None,
+        },
+        {
+            "id": "tools",
+            "name": "tools",
+            "metadata": None,
+        },
+        {
+            "id": "__end__",
+            "name": "__end__",
+            "metadata": None,
+        },
+    ]
+
+    workflow_edges = json.loads(
+        workflow_span.attributes["lmnr.association.properties.langgraph.edges"]
+    )
+    assert all(
+        edge in workflow_edges
+        for edge in [
+            {"source": "__start__", "target": "assistant", "conditional": False},
+            {"source": "assistant", "target": "tools", "conditional": True},
+            {"source": "assistant", "target": "__end__", "conditional": True},
+            {"source": "tools", "target": "assistant", "conditional": False},
+        ]
+    )
+    assert len(workflow_edges) == 4
+
+    for other_span in other_spans:
+        assert (
+            other_span.attributes.get("lmnr.association.properties.langgraph.nodes")
+            is None
+        )
+        assert (
+            other_span.attributes.get("lmnr.association.properties.langgraph.edges")
+            is None
+        )
+
+
+@pytest.mark.asyncio
+async def test_langchain_langgraph_async(exporter: InMemorySpanExporter):
+    graph_builder = StateGraph(AgentState)
+    graph_builder.add_node("assistant", assistant)
+    graph_builder.add_node("tools", ToolNode(tools))
+    graph_builder.add_edge(START, "assistant")
+    graph_builder.add_conditional_edges("assistant", tools_condition)
+    graph_builder.add_edge("tools", "assistant")
+    graph = graph_builder.compile()
+
+    await graph.ainvoke(
+        {"messages": [HumanMessage(content="What is the weather in Tokyo?")]}
+    )
+
+    spans = exporter.get_finished_spans()
+    assert len(spans) == 3
+    workflow_span = [span for span in spans if span.name == "LangGraph.workflow"][0]
+    other_spans = [span for span in spans if span.name != "LangGraph.workflow"]
+    assert json.loads(
+        workflow_span.attributes["lmnr.association.properties.langgraph.nodes"]
+    ) == [
+        {
+            "id": "__start__",
+            "name": "__start__",
+            "metadata": None,
+        },
+        {
+            "id": "assistant",
+            "name": "assistant",
+            "metadata": None,
+        },
+        {
+            "id": "tools",
+            "name": "tools",
+            "metadata": None,
+        },
+        {
+            "id": "__end__",
+            "name": "__end__",
+            "metadata": None,
+        },
+    ]
+
+    workflow_edges = json.loads(
+        workflow_span.attributes["lmnr.association.properties.langgraph.edges"]
+    )
+    assert all(
+        edge in workflow_edges
+        for edge in [
+            {"source": "__start__", "target": "assistant", "conditional": False},
+            {"source": "assistant", "target": "tools", "conditional": True},
+            {"source": "assistant", "target": "__end__", "conditional": True},
+            {"source": "tools", "target": "assistant", "conditional": False},
+        ]
+    )
+    assert len(workflow_edges) == 4
+
+    for other_span in other_spans:
+        assert (
+            other_span.attributes.get("lmnr.association.properties.langgraph.nodes")
+            is None
+        )
+        assert (
+            other_span.attributes.get("lmnr.association.properties.langgraph.edges")
+            is None
+        )
+
+
+def test_langchain_langgraph_stream(exporter: InMemorySpanExporter):
+    graph_builder = StateGraph(AgentState)
+    graph_builder.add_node("assistant", assistant)
+    graph_builder.add_node("tools", ToolNode(tools))
+    graph_builder.add_edge(START, "assistant")
+    graph_builder.add_conditional_edges("assistant", tools_condition)
+    graph_builder.add_edge("tools", "assistant")
+    graph = graph_builder.compile()
+
+    for chunk in graph.stream(
+        {"messages": [HumanMessage(content="What is the weather in Tokyo?")]}
+    ):
+        pass
+
+    spans = exporter.get_finished_spans()
+    assert len(spans) == 3
+    workflow_span = [span for span in spans if span.name == "LangGraph.workflow"][0]
+    other_spans = [span for span in spans if span.name != "LangGraph.workflow"]
+    assert json.loads(
+        workflow_span.attributes["lmnr.association.properties.langgraph.nodes"]
+    ) == [
+        {
+            "id": "__start__",
+            "name": "__start__",
+            "metadata": None,
+        },
+        {
+            "id": "assistant",
+            "name": "assistant",
+            "metadata": None,
+        },
+        {
+            "id": "tools",
+            "name": "tools",
+            "metadata": None,
+        },
+        {
+            "id": "__end__",
+            "name": "__end__",
+            "metadata": None,
+        },
+    ]
+
+    workflow_edges = json.loads(
+        workflow_span.attributes["lmnr.association.properties.langgraph.edges"]
+    )
+    assert all(
+        edge in workflow_edges
+        for edge in [
+            {"source": "__start__", "target": "assistant", "conditional": False},
+            {"source": "assistant", "target": "tools", "conditional": True},
+            {"source": "assistant", "target": "__end__", "conditional": True},
+            {"source": "tools", "target": "assistant", "conditional": False},
+        ]
+    )
+    assert len(workflow_edges) == 4
+
+    for other_span in other_spans:
+        assert (
+            other_span.attributes.get("lmnr.association.properties.langgraph.nodes")
+            is None
+        )
+        assert (
+            other_span.attributes.get("lmnr.association.properties.langgraph.edges")
+            is None
+        )
+
+
+@pytest.mark.asyncio
+async def test_langchain_langgraph_async_stream(exporter: InMemorySpanExporter):
+    graph_builder = StateGraph(AgentState)
+    graph_builder.add_node("assistant", assistant)
+    graph_builder.add_node("tools", ToolNode(tools))
+    graph_builder.add_edge(START, "assistant")
+    graph_builder.add_conditional_edges("assistant", tools_condition)
+    graph_builder.add_edge("tools", "assistant")
+    graph = graph_builder.compile()
+
+    async for chunk in graph.astream(
+        {"messages": [HumanMessage(content="What is the weather in Tokyo?")]}
+    ):
+        pass
 
     spans = exporter.get_finished_spans()
     assert len(spans) == 3
