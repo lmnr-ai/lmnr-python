ea37581918e03d9cc39b2253b624ab7c6fb53551
Robert Kim <skull8888888@gmail.com>
2025-06-29 14:29:02 +0100

    removed ignore

--- src/lmnr/sdk/browser/skyvern_otel.py
+++ src/lmnr/sdk/browser/skyvern_otel.py
@@ -27,8 +27,6 @@ WRAPPED_METHODS = [
         "object": "Skyvern",  # Class name
         "method": "run_task", # Method name
         "span_name": "Skyvern.run_task",
-        "ignore_input": False,
-        "ignore_output": False,
         "span_type": "DEFAULT",
     },
     {
@@ -36,8 +34,6 @@ WRAPPED_METHODS = [
         # No "object" field for module-level functions
         "method": "get_interactable_element_tree", # Function name
         "span_name": "get_interactable_element_tree",
-        "ignore_input": False,
-        "ignore_output": False,
         "span_type": "DEFAULT",
     },
     {
@@ -45,8 +41,6 @@ WRAPPED_METHODS = [
         "object": "ForgeAgent",
         "method": "execute_step",
         "span_name": "ForgeAgent.execute_step",
-        "ignore_input": False,
-        "ignore_output": False,
         "span_type": "DEFAULT",
     }
 ]
@@ -59,22 +53,21 @@ async def _wrap(tracer: Tracer, to_wrap, wrapped, instance, args, kwargs):
         "lmnr.span.type": to_wrap.get("span_type"),
     }
     
-    if not to_wrap.get("ignore_input"):
-        attributes["lmnr.span.input"] = json_dumps(
-            get_input_from_func_args(wrapped, True, args, kwargs)
-        )
+    attributes["lmnr.span.input"] = json_dumps(
+        get_input_from_func_args(wrapped, True, args, kwargs)
+    )
     
     with tracer.start_as_current_span(span_name) as span:
         span.set_attributes(attributes)
         result = await wrapped(*args, **kwargs)
-        if not to_wrap.get("ignore_output"):
-            to_serialize = result
-            serialized = (
-                to_serialize.model_dump_json()
-                if isinstance(to_serialize, pydantic.BaseModel)
-                else json_dumps(to_serialize)
-            )
-            span.set_attribute("lmnr.span.output", serialized)
+
+        to_serialize = result
+        serialized = (
+            to_serialize.model_dump_json()
+            if isinstance(to_serialize, pydantic.BaseModel)
+            else json_dumps(to_serialize)
+        )
+        span.set_attribute("lmnr.span.output", serialized)
         return result
 
 
